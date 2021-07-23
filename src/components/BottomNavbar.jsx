068a5f4e3096acfd14fdaae6369a8123c7816ba3
Abadi Suryo <abadisuryosetiyo@gmail.com>
2021-07-23 22:29:02 +0700

    refine BottomNavbar

--- src/components/BottomNavbar.jsx
+++ src/components/BottomNavbar.jsx
@@ -1,27 +1,73 @@
 import React from 'react'
-import {AccessibilityNew, Schedule, Notifications, AccountCircle } from '@material-ui/icons';
+import {AccessibilityNew, Schedule, Notifications, AccountCircle, AddToQueue } from '@material-ui/icons';
 import { Link } from 'react-router-dom';
 const BottomNavbar = () => {
+
+    const items = [
+        {
+            'name': 'Beranda',
+            'path': '/',
+            'icon': <AccessibilityNew/>
+        },
+        {
+            'name': 'Schedule',
+            'path': '/schedule',
+            'icon': <Schedule/>
+        },
+        {
+            'name': 'Registrasi',
+            'path': '/registration',
+            'icon': <AddToQueue/>
+        },
+        {
+            'name': 'Notifikasi',
+            'path': '/notification',
+            'icon': <Notifications/>
+        },
+        {
+            'name': 'Profile',
+            'path': '/profile',
+            'icon': <AccountCircle/>
+        },
+        {
+            'name': 'Beranda',
+            'path': '/',
+            'icon': <AccessibilityNew/>
+        },
+        {
+            'name': 'Schedule',
+            'path': '/schedule',
+            'icon': <Schedule/>
+        },
+        {
+            'name': 'Registrasi',
+            'path': '/registration',
+            'icon': <AddToQueue/>
+        },
+        {
+            'name': 'Notifikasi',
+            'path': '/notification',
+            'icon': <Notifications/>
+        },
+        {
+            'name': 'Profile',
+            'path': '/profile',
+            'icon': <AccountCircle/>
+        },
+    ]
+
     return (
         <div className="position-fixed bottom-0 w-100 d-flex justify-content-center mb-3">
-            <div className="bg-transparent-50 shadow p-2 px-4 rounded-4 hover-scale transition-02">
-                <div className="d-flex justify-content-center">
-                    <Link to="/" className="btn btn-light rounded-4 mx-1 col-3 hover-scale transition-02">
-                        <AccessibilityNew/><br/>
-                        <small className="ft-tiny">Beranda</small>
-                    </Link>
-                    <Link to="/schedule" className="btn btn-light rounded-4 mx-1 col-3 hover-scale transition-02">
-                        <Schedule/><br/>
-                        <small className="ft-tiny">Schedule</small>
-                    </Link>
-                    <Link to="/notification" className="btn btn-light rounded-4 mx-1 col-3 hover-scale transition-02">
-                        <Notifications/><br/>
-                        <small className="ft-tiny">Notifikasi</small>
-                    </Link>
-                    <Link to="/profile" className="btn btn-light rounded-4 mx-1 col-3 hover-scale transition-02">
-                        <AccountCircle/><br/>
-                        <small className="ft-tiny">Profile</small>
-                    </Link>
+            <div className="bg-transparent-50 shadow p-2 px-3 rounded-4 hover-scale transition-02">
+                <div className="d-flex justify-content-center row">
+                    {items.map((item, index)=>{
+                        return (
+                            <Link to={item.path} className="btn btn-light rounded-4 mx-1 mb-1 col hover-scale transition-02 width-4">
+                                {item.icon}<br/>
+                                <small className="ft-tiny">{item.name}</small>
+                            </Link>
+                        )
+                    })}
                 </div>
             </div>
         </div>
